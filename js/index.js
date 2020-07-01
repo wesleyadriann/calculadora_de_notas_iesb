76450bc0c98fd95ecc23313d5c4f7406d0f524ac
Wesley Adriann <wesleyadriann@gmail.com>
2020-06-30 22:20:29 -0300

    Adds theme switch
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
 
-function App() {
-    
+function changeThemeListener() {
+    const themeSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+    themeSwitch.addEventListener('change', (e) => {
+        const theme = e.target.checked ? 'dark' : 'light';
+        document.documentElement.setAttribute('data-theme', theme);
+    });
 }
+
+function calculaNotas(event) {
+    console.log(event.target.id, event.target.value);
+}
+
+function App() {
+    changeThemeListener();
+    
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach((input) => input.addEventListener('change', calculaNotas));
+}
+
 
 App();
