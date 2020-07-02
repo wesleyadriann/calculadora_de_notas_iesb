470a8db7b1335611c60877420af552de5b2a2672
Wesley Adriann <wesleyadriann@gmail.com>
2020-07-01 21:21:46 -0300

    Adds a1 and a2 calculator
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- js/calculaNotas.js
+++ js/calculaNotas.js
@@ -1,8 +1,8 @@
 
 function calculaA1() {
-    const a1wrapper = document.querySelector('#A1');
-    const [inputA1, inputObj] = a1wrapper.querySelectorAll('input');
-    const paragraph = a1wrapper.querySelector('p');
+    const wrapper = document.querySelector('#A1');
+    const [inputA1, inputObj] = wrapper.querySelectorAll('input');
+    const paragraph = wrapper.querySelector('p');
     if(inputA1.value && inputObj.value) {
         const A2 = ((parseFloat(inputObj.value) - (parseFloat(inputA1.value) *0.4))/0.6).toFixed(2);
         const text = `É necessário tirar ${A2} na A2 para ficar com a média de ${inputObj.value}.`;
@@ -11,7 +11,25 @@ function calculaA1() {
 }
 
 function calculaA1A2() {
-    console.log('clicou in a1 a2');
+    const wrapper = document.querySelector('#A1A2');
+    const [inputA1, inputA2] = wrapper.querySelectorAll('input');
+    const paragraph = wrapper.querySelector('p');
+
+    if(inputA1.value && inputA2.value) {
+        let text;
+        const average = ((parseFloat(inputA1.value) * 0.4) + (parseFloat(inputA2.value) * 0.6)).toFixed(2);
+
+        if(average > 5) {
+            text = `Parabéns você foi aprovado com a média ${average}`;
+        } else {
+            const a1sub = ((5 - (parseFloat(inputA2.value) * 0.6))/0.4).toFixed(2);
+            const a2sub = ((5 - (parseFloat(inputA1.value) * 0.4))/0.6).toFixed(2);
+            const a3 = a1sub > a2sub ? a2sub : a1sub;
+            text = `É necessário fazer a A3 e tirar ${a3} para ficar com a média de 5`; 
+        }
+
+        paragraph.innerText = text;
+    }
 }
 
 function calculaA3() {
