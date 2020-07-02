127fcfa9cefd15cdc61706a23506146ebeb5dd82
Wesley Adriann <wesleyadriann@gmail.com>
2020-07-01 22:55:22 -0300

    Adds A3 calculator
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- js/calculaNotas.js
+++ js/calculaNotas.js
@@ -4,8 +4,9 @@ function calculaA1() {
     const [inputA1, inputObj] = wrapper.querySelectorAll('input');
     const paragraph = wrapper.querySelector('p');
     if(inputA1.value && inputObj.value) {
-        const A2 = ((parseFloat(inputObj.value) - (parseFloat(inputA1.value) *0.4))/0.6).toFixed(2);
-        const text = `É necessário tirar ${A2} na A2 para ficar com a média de ${inputObj.value}.`;
+        const A2 = ((parseFloat(inputObj.value) - (parseFloat(inputA1.value) * 0.4))/0.6);
+        const text = `É necessário tirar ${A2.toFixed(2)} na A2 para ficar com a média de ${inputObj.value} .`;
+
         paragraph.innerText = text;
     }
 }
@@ -17,15 +18,14 @@ function calculaA1A2() {
 
     if(inputA1.value && inputA2.value) {
         let text;
-        const average = ((parseFloat(inputA1.value) * 0.4) + (parseFloat(inputA2.value) * 0.6)).toFixed(2);
-
-        if(average > 5) {
-            text = `Parabéns você foi aprovado com a média ${average}`;
+        const average = ((parseFloat(inputA1.value) * 0.4) + (parseFloat(inputA2.value) * 0.6));
+        if(average >= 5) {
+            text = `Parabéns você foi aprovado com a média ${average.toFixed(2)} .`;
         } else {
-            const a1sub = ((5 - (parseFloat(inputA2.value) * 0.6))/0.4).toFixed(2);
-            const a2sub = ((5 - (parseFloat(inputA1.value) * 0.4))/0.6).toFixed(2);
-            const a3 = a1sub > a2sub ? a2sub : a1sub;
-            text = `É necessário fazer a A3 e tirar ${a3} para ficar com a média de 5`; 
+            const a1sub = ((5 - (parseFloat(inputA2.value) * 0.6))/0.4);
+            const a2sub = ((5 - (parseFloat(inputA1.value) * 0.4))/0.6);
+            const a3 = Math.max(...[a1sub, a2sub]);
+            text = `É necessário fazer a A3 e tirar ${a3.toFixed(2)} para ficar com a média de 5 .`; 
         }
 
         paragraph.innerText = text;
@@ -33,7 +33,23 @@ function calculaA1A2() {
 }
 
 function calculaA3() {
-    console.log('clicou in a3');
+    const wrapper = document.querySelector('#A3');
+    const [inputA1, inputA2, inputA3] = wrapper.querySelectorAll('input');
+    const paragraph = wrapper.querySelector('p');
+
+    if(inputA1.value && inputA2.value && inputA3.value) {
+        let text;
+        const averageA1 = ((parseFloat(inputA1.value) * 0.4) + (parseFloat(inputA3.value) * 0.6));
+        const averageA2 = ((parseFloat(inputA2.value) * 0.6) + (parseFloat(inputA3.value) * 0.4));
+        const average = Math.max(...[averageA1, averageA2]);
+        if(average >= 5) {
+            text = `Parabéns você foi aprovado com a média ${average.toFixed(2)} .`;
+        } else {
+            text = 'Você esta reprovado.'
+        }
+
+        paragraph.innerText = text;
+    }
 }
 
 function buttonsEvent(event) {
